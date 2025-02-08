a48d5deb232fc02f6701335f40b365fa8c2c99e9
Yifan Zhang <zhangyif21@mails.tsinghua.edu.cn>
2025-02-09 05:47:53 +0800

    minor

---
+++ eval_4gpu.sh
@@ -0,0 +1,32 @@
+NUM_GPUS=4
+# Check if a parameter is provided
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <model_name>"
+    exit 1
+fi
+
+# Get the passed parameter
+MODEL_NAME=$1
+
+# Construct the model path using the provided model name
+MODEL="../X0/ckpt/${MODEL_NAME}/global_step_12"
+# Set the model arguments
+MODEL_ARGS="pretrained=$MODEL,dtype=bfloat16,tensor_parallel_size=$NUM_GPUS,max_model_length=36864,gpu_memory_utilisation=0.8"
+# Define the output directory
+OUTPUT_DIR="data/evals/$MODEL"
+
+# AIME 2024
+#TASK=aime24
+# Run lighteval for the AIME 2024 task (currently commented out)
+#lighteval vllm $MODEL_ARGS "custom|$TASK|0|0" \
+#    --custom-tasks src/open_r1/evaluate.py \
+#    --use-chat-template \
+#    --output-dir $OUTPUT_DIR
+
+# MATH - 500
+TASK=math_500
+# Run lighteval for the MATH-500 task
+lighteval vllm $MODEL_ARGS "custom|$TASK|0|0" \
+    --custom-tasks src/open_r1/evaluate.py \
+    --use-chat-template \
+    --output-dir $OUTPUT_DIR
